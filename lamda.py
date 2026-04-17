92f080939a2be9a259847268a3d96c34058f34ca
ranjeetkanojiya39 <ranjeetskanojiya20@gmail.com>
2026-04-18 00:40:59 +0530

    update

---
+++ lamda.py
@@ -0,0 +1,6 @@
+# Use a lambda function to square all elements in a list. 
+
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = list(map(lambda x: x ** 2, numbers))
+print(squared_numbers)  # Output: [1, 4, 9, 16, 25]
